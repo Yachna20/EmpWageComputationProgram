1e75a829b18a980f21aa93cdacfdbfc35c98c84b
Yachna20 <thakreyachna20@gmail.com>
2022-11-23 01:17:03 +0530

    Added:Calculate Wages til no of working days or total working hrs per momth is Reached

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,28 +1,38 @@
 echo "Welcome to Employee Wage Computation Program"
-isPartTime=1;
-isFullTime=2;
-numofWorkingDays=20;
+perHourSalary=20;
+workingHour=0;
 totalSalary=0;
-empRatePerHr=20;
-
-for (( day=1; day<=20 ; day++ ))
+totalWorkingHour=0;
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
-	empCheck=$((RANDOM%3));
-	case $empCheck in
-		$isFulltTime)
-		 empHrs=8
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
 		;;
-               $isPartTime)
-	         empHrs=4
-	        ;;
-                *)
-                 empHrs=0
-	        ;;
-        esac
-	salary=$(($empHrs*$empRatePerHr));
-	totalSalary=$(($totalSalary + $salary));
-done
 
-echo "Employee has earned $totalSalary $ in a month";
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	if [ $totalWorkingHour -gt 100 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		break;
+	fi
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
 
 
